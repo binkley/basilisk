7eff4225648cf16061f81efe3255af5cf63db0ce
Brian Oxley <boxley@thoughtworks.com>
2019-01-14 13:00:39 -0600

    Basic application

---
+++ src/main/java/hm/binkley/basilisk/Application.java
@@ -0,0 +1,12 @@
+package hm.binkley.basilisk;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class Application {
+    public static void main(final String... args) {
+        run(Application.class, args);
+    }
+}
