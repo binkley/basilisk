abbb95e65d34ab405da98594a396a323e0d492fc
Brian Oxley <boxley@thoughtworks.com>
2019-01-14 19:47:52 -0600

    Example with streamed repository

--- src/databaseTest/java/hm/binkley/basilisk/store/BasiliskRepositoryTest.java
+++ src/databaseTest/java/hm/binkley/basilisk/store/BasiliskRepositoryTest.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,5 +28,24 @@ class BasiliskRepositoryTest {
                 .findById(repository.save(unsaved).getId());
 
         assertThat(found).contains(unsaved);
+    }
+
+    @Test
+    void shouldStream() {
+        final BasiliskRecord unsavedA = BasiliskRecord.builder()
+                .word("BIRD")
+                .when(Instant.ofEpochSecond(1_000_000))
+                .build();
+        final BasiliskRecord unsavedB = BasiliskRecord.builder()
+                .word("WORD")
+                .when(Instant.ofEpochSecond(1_000_000))
+                .build();
+        repository.saveAll(List.of(unsavedA, unsavedB));
+
+        // Wrap in try-with-resources to close the stream when done; this
+        // frees up DB resources as a stream is potentially very long
+        try (final Stream<BasiliskRecord> found = repository.readAll()) {
+            assertThat(found).containsExactly(unsavedA, unsavedB);
+        }
     }
 }
--- src/main/java/hm/binkley/basilisk/store/BasiliskRepository.java
+++ src/main/java/hm/binkley/basilisk/store/BasiliskRepository.java
@@ -1,10 +1,16 @@
 package hm.binkley.basilisk.store;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public interface BasiliskRepository
-        extends CrudRepository<BasiliskRecord, Long> {
+        extends PagingAndSortingRepository<BasiliskRecord, Long> {
     List<BasiliskRecord> findByWord(String word);
+
+    // An example of both 1) a custom query, and 2) returning a stream
+    @Query("SELECT * FROM BASILISK.BASILISKS")
+    Stream<BasiliskRecord> readAll();
 }
