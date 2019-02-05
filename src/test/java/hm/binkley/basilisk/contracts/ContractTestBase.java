d51432f1c58e152891825f7c650467e90fe95d0f
Brian Oxley <boxley@thoughtworks.com>
2019-02-04 20:54:52 -0600

    Basic contract test

---
+++ src/test/java/hm/binkley/basilisk/contracts/ContractTestBase.java
@@ -0,0 +1,21 @@
+package hm.binkley.basilisk.contracts;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.web.context.WebApplicationContext;
+
+@AutoConfigureEmbeddedDatabase
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public abstract class ContractTestBase {
+    @Autowired
+    protected WebApplicationContext context;
+
+    @BeforeEach
+    void setUp() {
+        RestAssuredMockMvc.webAppContextSetup(context);
+    }
+}
