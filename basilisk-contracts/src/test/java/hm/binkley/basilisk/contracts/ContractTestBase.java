36ad2fe91a006b9bbaeaecce9c93c141555adc8f
Brian Oxley <boxley@thoughtworks.com>
2019-02-08 07:36:26 -0600

    Contract tests separate
    First pass: cleaning required.

--- basilisk-contracts/src/test/java/hm/binkley/basilisk/contracts/ContractTestBase.java
+++ basilisk-contracts/src/test/java/hm/binkley/basilisk/contracts/ContractTestBase.java
@@ -1,16 +1,19 @@
 package hm.binkley.basilisk.contracts;
 
+import hm.binkley.basilisk.BasiliskApplication;
 import io.restassured.RestAssured;
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ContextConfiguration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @AutoConfigureEmbeddedDatabase
+@ContextConfiguration(classes = BasiliskApplication.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 abstract class ContractTestBase {
     @LocalServerPort
