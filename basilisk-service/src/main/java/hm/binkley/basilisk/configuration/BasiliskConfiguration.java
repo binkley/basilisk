d17d8fedde73a623c4d67720af98d547ce364432
Brian Oxley <boxley@thoughtworks.com>
2019-02-08 06:43:37 -0600

    Thinner configuration slicing; re-enable PMD

--- basilisk-service/src/main/java/hm/binkley/basilisk/configuration/BasiliskConfiguration.java
+++ basilisk-service/src/main/java/hm/binkley/basilisk/configuration/BasiliskConfiguration.java
@@ -1,14 +1,7 @@
 package hm.binkley.basilisk.configuration;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
-@EnableConfigurationProperties({
-        BasiliskProperties.class,
-        OverlappedProperties.class})
 public class BasiliskConfiguration {
 }
--- basilisk-service/src/main/java/hm/binkley/basilisk/configuration/ProblemConfiguration.java
+++ basilisk-service/src/main/java/hm/binkley/basilisk/configuration/ProblemConfiguration.java
@@ -1,7 +1,9 @@
 package hm.binkley.basilisk.configuration;
 
 import lombok.Generated;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.problem.ProblemModule;
@@ -10,6 +12,7 @@ import org.zalando.problem.validation.ConstraintViolationProblemModule;
 import static org.springframework.boot.autoconfigure.web.ErrorProperties.IncludeStacktrace.ALWAYS;
 
 @Configuration
+@EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
 @Generated // Lie to JaCoCo
 public class ProblemConfiguration {
     private static boolean includeStackTrace(final ServerProperties server) {
