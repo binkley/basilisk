ff40f9219131c5bbf169f9fb87984ab874c2a7dd
Brian Oxley <boxley@thoughtworks.com>
2019-02-20 17:22:57 -0600

    Abstract store

--- basilisk-service/src/main/java/hm/binkley/basilisk/domain/store/BasiliskRecord.java
+++ basilisk-service/src/main/java/hm/binkley/basilisk/domain/store/BasiliskRecord.java
@@ -1,7 +1,9 @@
 package hm.binkley.basilisk.domain.store;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.Wither;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
@@ -9,17 +11,22 @@ import org.springframework.lang.NonNull;
 
 import java.time.Instant;
 
-@Data
 @EqualsAndHashCode(exclude = {"id", "receivedAt"})
+@RequiredArgsConstructor
 @Table("BASILISK.BASILISK")
+@ToString
 public final class BasiliskRecord {
     @Id
+    @Getter
     @Wither
     private final Long id;
+    @Getter
     @Wither
     private final Instant receivedAt;
+    @Getter
     @NonNull
     private final String word;
+    @Getter
     @NonNull
     private final Instant at;
 
