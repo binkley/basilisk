56c5946f0750f01fd56a66b69dc6638fc9f20af6
Brian Oxley <boxley@thoughtworks.com>
2019-03-22 13:18:18 -0500

    Starting point to refactor standard record

---
+++ basilisk-service/src/test/java/hm/binkley/basilisk/store/StandardRecordTest.java
@@ -0,0 +1,32 @@
+package hm.binkley.basilisk.store;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+@RequiredArgsConstructor
+class StandardRecordTest {
+    @Mock
+    private final MyTestStore store;
+
+    private MyTestRecord record;
+
+    @BeforeEach
+    void setUp() {
+        record = new MyTestRecord(null, null, "ABC", 123);
+        record.store = store;
+    }
+
+    @Test
+    void shouldSave() {
+        record.save();
+
+        verify(store).save(record);
+    }
+}
