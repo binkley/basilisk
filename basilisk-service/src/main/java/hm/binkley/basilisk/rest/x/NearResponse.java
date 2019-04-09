08618387f8bef27855da15ea34343e61e25e4650
Brian Oxley <boxley@thoughtworks.com>
2019-04-09 16:51:24 -0500

    Oopsie

--- basilisk-service/src/main/java/hm/binkley/basilisk/rest/x/NearResponse.java
+++ basilisk-service/src/main/java/hm/binkley/basilisk/rest/x/NearResponse.java
@@ -4,10 +4,16 @@ import hm.binkley.basilisk.store.x.Near;
 import lombok.Value;
 
 @Value
-public final class NearResponse {
+public final class NearResponse
+        implements Comparable<NearResponse> {
     String code;
 
     public static NearResponse of(final Near near) {
         return new NearResponse(near.getCode());
+    }
+
+    @Override
+    public int compareTo(final NearResponse that) {
+        return code.compareTo(that.code);
     }
 }
