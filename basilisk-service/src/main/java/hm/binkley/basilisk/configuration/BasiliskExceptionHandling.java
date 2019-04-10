01f0d0f8a44472b743b012c7c265a6363e981f71
Brian Oxley <boxley@thoughtworks.com>
2019-04-10 17:15:49 -0500

    Problems for security

--- basilisk-service/src/main/java/hm/binkley/basilisk/configuration/BasiliskExceptionHandling.java
+++ basilisk-service/src/main/java/hm/binkley/basilisk/configuration/BasiliskExceptionHandling.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.zalando.problem.StatusType;
 import org.zalando.problem.spring.web.advice.ProblemHandling;
+import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
 
 import static org.springframework.boot.autoconfigure.web.ErrorProperties.IncludeStacktrace.ALWAYS;
 import static org.zalando.problem.Status.UNPROCESSABLE_ENTITY;
@@ -15,7 +16,7 @@ import static org.zalando.problem.Status.UNPROCESSABLE_ENTITY;
 @Generated
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class BasiliskExceptionHandling
-        implements ProblemHandling {
+        implements ProblemHandling, SecurityAdviceTrait {
     private final ServerProperties server;
 
     private static boolean includeStackTrace(final ServerProperties server) {
