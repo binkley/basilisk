52758ed1d9f8f60d5104826b366b3d2b35aae311
Brian Oxley <boxley@thoughtworks.com>
2019-04-13 07:59:47 -0500

    Cleverly find JSON test files

--- basilisk-service/src/integrationTest/java/hm/binkley/basilisk/x/near/rest/NearsControllerValidationTest.java
+++ basilisk-service/src/integrationTest/java/hm/binkley/basilisk/x/near/rest/NearsControllerValidationTest.java
@@ -14,10 +14,13 @@ import org.springframework.core.io.ClassRelativeResourceLoader;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.IOException;
+import java.lang.StackWalker.StackFrame;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import static hm.binkley.basilisk.x.TestFixtures.fixedNear;
+import static java.lang.StackWalker.Option.RETAIN_CLASS_REFERENCE;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.spy;
@@ -40,6 +43,50 @@ class NearsControllerValidationTest {
 
     private String nearCode;
 
+    private static String from(final String jsonFile)
+            throws IOException {
+        final var stackWalker
+                = StackWalker.getInstance(RETAIN_CLASS_REFERENCE);
+        final var testClass = stackWalker.getCallerClass();
+
+        final var loader = new ClassRelativeResourceLoader(testClass);
+        // It's a Jedi mind trick
+        return new Scanner(
+                loader.getResource(jsonFile).readableChannel(), UTF_8)
+                .useDelimiter("\\A")
+                .next();
+    }
+
+    private static String fromX(final String tag)
+            throws IOException {
+        final var stackWalker
+                = StackWalker.getInstance(RETAIN_CLASS_REFERENCE);
+        final StackFrame testFrame = stackWalker.walk(stacks -> stacks
+                .filter(it -> it.getMethodName().startsWith("should"))
+                .findFirst()
+                .orElseThrow());
+        final var testClass = testFrame.getDeclaringClass();
+
+        final var pattern = Pattern.compile("([A-Z])");
+        var matcher = pattern.matcher(testClass.getSimpleName());
+        final var filePrefix = matcher
+                .replaceAll(mr -> "-" + mr.group(1).toLowerCase())
+                .substring(1);
+        matcher = pattern.matcher(testFrame.getMethodName());
+        final var fileSuffix = matcher
+                .replaceAll(mr -> "-" + mr.group(1).toLowerCase())
+                .substring("should-".length());
+        final var jsonFile
+                = filePrefix + "-" + fileSuffix + "-" + tag + ".json";
+
+        // It's a Jedi mind trick
+        return new Scanner(new ClassRelativeResourceLoader(testClass)
+                .getResource(jsonFile)
+                .readableChannel(), UTF_8)
+                .useDelimiter("\\A")
+                .next();
+    }
+
     @BeforeEach
     void setUp() {
         final var near = fixedNear();
@@ -52,8 +99,7 @@ class NearsControllerValidationTest {
     void shouldComplainOnPostOneInvalid()
             throws Exception {
         controller.perform(post("/nears/post")
-                .content(from("nears-controller-validation-test-complain"
-                        + "-on-post-one-invalid-request.json"))
+                .content(fromX("request"))
                 .contentType(APPLICATION_JSON_UTF8))
                 .andExpect(status().isUnprocessableEntity())
                 .andExpect(content()
@@ -64,21 +110,10 @@ class NearsControllerValidationTest {
     void shouldComplainOnPutOneInvalid()
             throws Exception {
         controller.perform(put("/nears/put/" + nearCode)
-                .content(from("nears-controller-validation-test-complain"
-                        + "-on-put-one-invalid-request.json"))
+                .content(fromX("request"))
                 .contentType(APPLICATION_JSON_UTF8))
                 .andExpect(status().isUnprocessableEntity())
                 .andExpect(content()
                         .contentType(APPLICATION_PROBLEM_JSON));
-    }
-
-    private String from(final String jsonFile)
-            throws IOException {
-        final var loader = new ClassRelativeResourceLoader(getClass());
-        // It's a Jedi mind trick
-        return new Scanner(
-                loader.getResource(jsonFile).readableChannel(), UTF_8)
-                .useDelimiter("\\A")
-                .next();
     }
 }
