70e9ae1a0ef1ba960e6eb086c2a32a218b162c62
Brian Oxley <boxley@thoughtworks.com>
2019-04-13 07:28:30 -0500

    More problem RFC

--- basilisk-service/src/integrationTest/java/hm/binkley/basilisk/x/near/rest/NearsControllerValidationTest.java
+++ basilisk-service/src/integrationTest/java/hm/binkley/basilisk/x/near/rest/NearsControllerValidationTest.java
@@ -1,5 +1,6 @@
 package hm.binkley.basilisk.x.near.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import hm.binkley.basilisk.configuration.JsonConfiguration;
 import hm.binkley.basilisk.configuration.ProblemWebMvcTest;
 import hm.binkley.basilisk.x.near.Nears;
@@ -21,8 +22,10 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.spy;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
+import static org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Import(JsonConfiguration.class)
@@ -30,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 class NearsControllerValidationTest {
     private final MockMvc controller;
+    private final ObjectMapper objectMapper;
 
     @MockBean
     private Nears nears;
@@ -48,22 +52,24 @@ class NearsControllerValidationTest {
     void shouldComplainOnPostOneInvalid()
             throws Exception {
         controller.perform(post("/nears/post")
-                .content(from(
-                        "nears-controller-test-complain-on-post-one"
-                                + "-invalid-request.json"))
+                .content(from("nears-controller-validation-test-complain"
+                        + "-on-post-one-invalid-request.json"))
                 .contentType(APPLICATION_JSON_UTF8))
-                .andExpect(status().isUnprocessableEntity());
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(content()
+                        .contentType(APPLICATION_PROBLEM_JSON));
     }
 
     @Test
     void shouldComplainOnPutOneInvalid()
             throws Exception {
         controller.perform(put("/nears/put/" + nearCode)
-                .content(from(
-                        "nears-controller-test-complain-on-put-one"
-                                + "-invalid-request.json"))
+                .content(from("nears-controller-validation-test-complain"
+                        + "-on-put-one-invalid-request.json"))
                 .contentType(APPLICATION_JSON_UTF8))
-                .andExpect(status().isUnprocessableEntity());
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(content()
+                        .contentType(APPLICATION_PROBLEM_JSON));
     }
 
     private String from(final String jsonFile)
