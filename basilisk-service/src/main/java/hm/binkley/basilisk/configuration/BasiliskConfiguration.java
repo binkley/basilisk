d5ab68ce199479ddc6d60d654b775ffc7879b20f
Brian Oxley <boxley@thoughtworks.com>
2019-04-25 19:03:24 -0500

    Revert "Use aspects for transactions, not proxies"
    This reverts commit 926fd45badea3d9433b0a780ca850f6cbc4471bb.

--- basilisk-service/src/main/java/hm/binkley/basilisk/configuration/BasiliskConfiguration.java
+++ basilisk-service/src/main/java/hm/binkley/basilisk/configuration/BasiliskConfiguration.java
@@ -2,12 +2,8 @@ package hm.binkley.basilisk.configuration;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import static org.springframework.context.annotation.AdviceMode.ASPECTJ;
 
 @Configuration
 @EnableAdminServer
-@EnableTransactionManagement(mode = ASPECTJ)
 public class BasiliskConfiguration {
 }
